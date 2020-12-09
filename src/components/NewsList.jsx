cd99443d2e9dca85a5a2ebd5b96b958974652a53
rightdroid <rightdroid@gmail.com>
2020-12-09 16:24:12 +0200

    Bring db query from NewsList component  to page for consistency

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,27 +1,7 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
 import NewsItemCard from './NewsItemCard';
 import styled from '@emotion/styled';
 import { generatePath } from 'react-router-dom';
-import LoadingSpinner from './LoadingSpinner';
-
-const NEWS_ITEMS = gql`
-  query {
-    newsList(skip: 0 limit : 0) {
-      rows
-      {
-        id
-        title
-        img
-        url
-        comments
-        {
-          id
-        }
-      }
-    }
-}
-`;
 
 const NewsListWrapper = styled.div`
     display: grid;
@@ -29,28 +9,21 @@ const NewsListWrapper = styled.div`
     grid-gap: 25px;
 `;
 
-const NewsList = ({theme}) => {
-    const { loading, error, data } = useQuery(NEWS_ITEMS);
-    
-    if (loading) return <LoadingSpinner />;
-    if (error) return <p>Error</p>;
-    
-    return <NewsListWrapper>
-        {
-            data.newsList.rows.map( ({ id, title, img, url, comments }, i) => {
-                generatePath('/story-:id', {id : id});
-                return <NewsItemCard 
-                        key={id}
-                        id={id}
-                        url={url}
-                        title={title}
-                        img={img}
-                        commentsCount={comments.length}
-                        theme={theme}
-                    />
-            })
-        }
-        </NewsListWrapper>
-};
+const NewsList = ({theme, items}) => <NewsListWrapper>
+    {
+        items.map( ({ id, title, img, url, comments }, i) => {
+            generatePath('/story-:id', {id : id});
+            return <NewsItemCard 
+                    key={id}
+                    id={id}
+                    url={url}
+                    title={title}
+                    img={img}
+                    commentsCount={comments.length}
+                    theme={theme}
+                />
+        })
+    }
+    </NewsListWrapper>;
 
 export default NewsList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import NewsList from '../components/NewsList';
+import { useQuery, gql } from '@apollo/client';
+import LoadingSpinner from '../components/LoadingSpinner';
 
-const Home = ({theme}) => <NewsList theme={theme} />
+const NEWS_ITEMS = gql`
+  query {
+    newsList(skip: 0 limit : 0) {
+      rows
+      {
+        id
+        title
+        img
+        url
+        comments
+        {
+          id
+        }
+      }
+    }
+}
+`;
+
+const Home = ({theme}) => {
+    const { loading, error, data } = useQuery(NEWS_ITEMS);
+    
+    if (loading) return <LoadingSpinner />;
+    if (error) return <p>Error</p>;
+    
+    return <NewsList theme={theme} items={data.newsList.rows} />
+};
 
 export default Home;
