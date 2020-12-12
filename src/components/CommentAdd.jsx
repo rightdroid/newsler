e4bb3552475acfeb143861ac81b4447e0d0b48a5
rightdroid <rightdroid@gmail.com>
2020-12-12 20:00:48 +0200

    Add feedback to comment creation

--- src/components/CommentAdd.jsx
+++ src/components/CommentAdd.jsx
@@ -1,89 +1,101 @@
 import React from 'react';
-import { ListGroup, Form, Button } from 'react-bootstrap';
+import { ListGroup, Form, Button, Spinner } from 'react-bootstrap';
 import styled from '@emotion/styled';
-import { PersonCircle, Clock, HandThumbsUp } from 'react-bootstrap-icons';
+import { PersonCircle, Clock } from 'react-bootstrap-icons';
 import Moment from 'react-moment';
-import { gql, useMutation } from '@apollo/client';
-import { useState } from 'react';
+import { useMutation } from '@apollo/client';
 import queries from '../shared/constants/queries';
+
+const COMMENT_SENT_STATUS = [
+    {id: 0, responseText : ''}, // default, light
+    {id: 1, responseText : 'Comments Successfully Sent. Thank you!'}, // success
+    {id: 2, responseText : 'Oops! Error occurred: '}, // fail
+]
+Object.freeze(COMMENT_SENT_STATUS);
 
 const CommentAdd = ({theme, storyId}) => {
     
-    const [createComment, 
-        { loading, error }
-    ] = useMutation(queries.CREATE_COMMENT, 
-        {
-            update(cache, {data : {createComment}}){
-                cache.modify(
-                    {
-                        fields: {
-                            comments(existingTodos = []) {
-                                const newCommentRef = cache.writeFragment({
-                                    data: createComment,
-                                    fragment: gql`
-                                        fragment NewComment on Comment {
-                                        id
-                                        type
-                                        }
-                                    `
-                                });
-                              return [...existingTodos, newCommentRef];
-                            }
-                          }
-                    }
-                )
-            }
-        }
-    );
-    const [commentSent, setCommentSent] = useState(false);
     let emailInput, contentInput;
+    const [createComment, 
+        { data, loading, error }
+    ] = useMutation(queries.CREATE_COMMENT, 
+        // TODO make cache updating work
+        // {
+            // update(cache, {data : {createComment}}){
+            //     cache.modify(
+            //         {
+            //             fields: {
+            //                 comments(existingComments = []) {
+            //                     const newCommentRef = cache.writeFragment({
+            //                         data: createComment,
+            //                         fragment: gql`
+            //                             fragment NewComment on Comment {
+            //                             id
+            //                             type
+            //                             }
+            //                         `
+            //                     });
+            //                   return [...existingComments, newCommentRef];
+            //                 }
+            //               }
+            //         }
+            //     )
+            // }
+        // }
+    );
     
-    return commentSent ? <div style={{textAlign : 'center', padding : '40px 0'}}><HandThumbsUp 
-            style={{fontSize : '35px', color: 'var(--colorAccent)'}}/>
-        </div> : <ListGroup.Item style={{...themeStyle, display : 'grid', gridTemplateColumns : '2fr 6fr'}}>
+    const handleCreateComment = e => {
+        e.preventDefault();
+        
+        createComment({variables : 
+            {
+                input : {
+                    newsId : storyId,
+                    email : emailInput.value,
+                    content : contentInput.value,
+                }
+            }
+        }).then(({ data }) => {
+            // successful resolve
+            // clear input fields
+            emailInput.value = '';
+            contentInput.value = '';
+        })
+        .catch(e => {
+            // handle errors
+        });
+        
+    }
+    
+    return <ListGroup.Item style={{...themeStyle, display : 'grid', gridTemplateColumns : '2fr 6fr'}}>
         <ProfileImage>
             <PersonCircle style={{width : '100%', height : '100%'}}/>
         </ProfileImage>
         <CommentBody>
             <CommentBodyHeader><Clock /> <Moment /> </CommentBodyHeader>
             <CommentBodyContent>
-                <Form onSubmit={e => {
-                    e.preventDefault();
-                    createComment({variables : 
-                        {
-                            input : {
-                                newsId : storyId,
-                                email : emailInput.value,
-                                content : contentInput.value,
-                            }
-                        }
-                    });
-                    emailInput.value = '';
-                    contentInput.value = '';
-                    
-                    setCommentSent(true);
-                    setTimeout(() => {
-                        setCommentSent(false);
-                    }, 2000);
-                }} >
+                <Form onSubmit={handleCreateComment} >
                     <Form.Group controlId="formGroupEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" 
                             required ref={node => {emailInput = node}}/>
                     </Form.Group>
-                    <Form.Group controlId="formGroupPassword">
+                    <Form.Group controlId="formGroupContent">
                         <Form.Label>Comment</Form.Label>
-                        <Form.Control as="textarea" rows={3} placeholder="comment content..." 
+                        <Form.Control as="textarea" rows={3} placeholder="Enter comment content..." 
                             required ref={node => {contentInput = node}}/>
                     </Form.Group>
-                    <Button  type="submit" 
-                    variant={theme === 1 ? 'dark' : 'light'} 
-                    style={{...themeStyle, 
-                    border : '1px solid var(--colorAccent)',
-                    margin : '10px 0'}}>Submit</Button>
+                    <CommentFormFooter>
+                        <Button  type="submit" 
+                        variant={theme === 1 ? 'dark' : 'light'} 
+                        style={{...themeStyle, 
+                        border : '1px solid var(--colorAccent)',
+                        margin : '10px 0'}}>Submit</Button>
+                        {data && <p>{COMMENT_SENT_STATUS[1].responseText}</p>}
+                        {loading && <Spinner animation='grow' variant='warning' />}
+                        {error && <p>{COMMENT_SENT_STATUS[2].responseText} {error.message}</p>}
+                    </CommentFormFooter>
                 </Form>
-                {loading && <p>Loading...</p>}
-                {error && <p>Error :( Please try again</p>}
             </CommentBodyContent>
         </CommentBody>
     </ListGroup.Item>
@@ -107,6 +119,12 @@ const CommentBodyHeader = styled.div`
 `;
 const CommentBodyContent = styled.div`
 
+`;
+const CommentFormFooter = styled.div`
+    display : grid;
+    grid-template-columns : 1fr 4fr;
+    grid-gap: 25px;
+    align-items : center;
 `;
 
 const themeStyle = {
